fce6290de46c207e12d1edf0b6aaac046195e324
RH-Sowrov <sowrov.sml@gmail.com>
2024-04-27 12:20:04 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,130 @@
+let openShopping = document.querySelector('.shopping');
+let closeShopping = document.querySelector('.closeShopping');
+let list = document.querySelector('.list');
+let listCart = document.querySelector('.listCart');
+let body = document.querySelector('body');
+let total = document.querySelector('.total');
+let quantity = document.querySelector('.quantity');
+let searchInput = document.getElementById('searchInput');
+
+openShopping.addEventListener('click', ()=>{
+    body.classList.add('active');
+})
+closeShopping.addEventListener('click', ()=>{
+    body.classList.remove('active');
+})
+
+let products = [
+    {
+        id: 1,
+        name: 'Winter Hoddie',
+        image: '1.PNG',
+        price: 1200
+    },
+    {
+        id: 2,
+        name: 'Green Hoddie',
+        image: '2.PNG',
+        price: 1500
+    },
+    {
+        id: 3,
+        name: 'Track Jacket',
+        image: '3.PNG',
+        price: 1750
+    },
+    {
+        id: 4,
+        name: 'Leather Jacket',
+        image: '4.PNG',
+        price: 2950
+    },
+    {
+        id: 5,
+        name: 'All Season Hoodie',
+        image: '5.PNG',
+        price: 2000
+    },
+    {
+        id: 6,
+        name: 'Hoddie with Cap',
+        image: '6.PNG',
+        price: 1990
+    }
+];
+let listCarts  = [];
+function initApp(){
+    products.forEach((value, key) =>{
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('item');
+        newDiv.innerHTML = `
+            <img src="image/${value.image}">
+            <div class="title">${value.name}</div>
+            <div class="price">${value.price.toLocaleString()}</div>
+            <button onclick="addToCart(${key})">Add To Cart</button>`;
+        list.appendChild(newDiv);
+    })
+}
+initApp();
+function addToCart(key){
+    if(listCarts[key] == null){
+        listCarts[key] = JSON.parse(JSON.stringify(products[key]));
+        listCarts[key].quantity = 1;
+    }
+    reloadCart();
+}
+function reloadCart(){
+    listCart.innerHTML = '';
+    let count = 0;
+    let totalPrice = 0;
+    listCarts.forEach((value, key)=>{
+        totalPrice = totalPrice + value.price;
+        count = count + value.quantity;
+        if(value != null){
+            let newDiv = document.createElement('li');
+            newDiv.innerHTML = `
+                <div><img src="image/${value.image}"/></div>
+                <div>${value.name}</div>
+                <div>${value.price.toLocaleString()}</div>
+                <div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
+                    <div class="count">${value.quantity}</div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
+                </div>`;
+                listCart.appendChild(newDiv);
+        }
+    })
+    total.innerText = totalPrice.toLocaleString();
+    quantity.innerText = count;
+}
+function changeQuantity(key, quantity){
+    if(quantity == 0){
+        delete listCarts[key];
+    }else{
+        listCarts[key].quantity = quantity;
+        listCarts[key].price = quantity * products[key].price;
+    }
+    reloadCart();
+}
+
+    searchInput.addEventListener('input', handleSearch);
+    function handleSearch() {
+    let searchTerm = searchInput.value.toLowerCase();
+    let filteredProducts = products.filter(product =>
+        product.name.toLowerCase().includes(searchTerm)
+    );
+    list.innerHTML = '';
+
+    filteredProducts.forEach((value, key) => {
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('item');
+        newDiv.innerHTML = `
+            <img src="image/${value.image}">
+            <div class="title">${value.name}</div>
+            <div class="price">${value.price.toLocaleString()}</div>
+            <button onclick="addToCart(${key})">Add To Cart</button>`;
+        list.appendChild(newDiv);
+    });
+}
+
+handleSearch();
